70622dcc768ce7335279dfc7316bac0d59e679ed
bufander <bufanderbufander@gmail.com>
2022-08-01 09:12:32 +0200

    feat: added all base files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ape-strategy-mix
 Base repository to develop a Yearn VaultV3 Strategy
+
+pip install -r requirements-dev.txt
+yarn
+ape plugins install .
+ape compile
+ape test
---
+++ contracts/Strategy.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+pragma solidity 0.8.14;
+pragma experimental ABIEncoderV2;
+
+
+import {IERC20} from "@yearnvaultsv3/test/BaseStrategy.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract Strategy {
+
+}
