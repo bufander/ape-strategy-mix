19045897d47588798114bbd1230104ab47ad60c2
bufander <bufanderbufander@gmail.com>
2022-08-01 09:14:55 +0200

    fix: added linting

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -3,10 +3,7 @@
 pragma solidity 0.8.14;
 pragma experimental ABIEncoderV2;
 
-
 import {IERC20} from "@yearnvaultsv3/test/BaseStrategy.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
-contract Strategy {
-
-}
+contract Strategy {}
