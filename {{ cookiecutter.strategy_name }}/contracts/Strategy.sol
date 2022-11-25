01f294280284f32a41f82023bfbeb97a87d8965a
jmonteer <jmonteer@protonmail.com>
2022-11-25 19:25:25 +0100

    feat: improvements

--- {{ cookiecutter.strategy_name }}/contracts/Strategy.sol
+++ {{ cookiecutter.strategy_name }}/contracts/Strategy.sol
@@ -12,28 +12,61 @@ contract Strategy is BaseStrategy {
         BaseStrategy(_vault, _name)
     {}
 
-    // ******** OVERRIDE METHODS FROM BASE CONTRACT IF NEEDED ************
-    // ******** CREATE NEEDED METHODS FOR THE STRATEGY ************
+    function _totalAssets() internal view override returns (uint256) {
+        // report the total amount of assets that the strategy holds
+        // NOTE: output will be used by vault to evaluate the P&L of the strategy
+        // if totalAssets > currentDebt -> profit
+        // if totalAssets < currentDebt -> loss
+        // recommendation is to report only when it can be considered profit (i.e. exclude very volatile tokens that still need to be sold)
+        return IERC20(asset).balanceOf(address(this));
+    }
+
+    function _invest() internal override {
+        // implement logic for depositing in underlying protocol
+    }
+
+    function _withdraw(
+        uint256 amount
+    ) internal override returns (uint256) {
+        // implement logic for withdrawing funds from the underlying protocol
+        return amount;
+    }
+ 
+    function _tend() internal override {
+        // implement logic for taking care of the position
+        // e.g. Compounding, readjusting debt ratios, ...
+    }
+
+    function _tendTrigger() internal override returns (bool) {
+        // implement logic to tell keepers to call tend()
+    }
+
+    function _aprAfterDebtChange(int256 delta) internal override returns (uint256) {
+        // estimate APR
+
+        // default set to 0 to avoid misallocations (worst case scenario the strategy doesn't receive funds)
+        return 0;
+    }
+
     function _maxWithdraw(address owner)
         internal
         view
         override
         returns (uint256)
     {
+        // Default: the strategy is fully liquid
         return _totalAssets();
     }
 
-    function _withdraw(
-        uint256 amount,
-        address receiver,
-        address owner
-    ) internal override returns (uint256) {
-        return amount;
+    function _maxDeposit(address owner)
+        internal
+        view
+        override
+        returns (uint256)
+    {
+        // Default: there are no deposit limitations
+        return type(uint256).max;
     }
 
-    function _invest() internal override {}
 
-    function _totalAssets() internal view override returns (uint256) {
-        return IERC20(asset).balanceOf(address(this));
-    }
 }
